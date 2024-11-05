00dade2ca67dd4bf82e5b24231999c7d611fc4f3
Runar Fosse <rfosse01@gmail.com>
2024-11-05 12:31:04 +0100

    Implemented training function

---
+++ train.py
@@ -0,0 +1,63 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+from torch import nn, optim
+from torch.utils.data import DataLoader
+
+from ray import train, tune
+
+from datasets import ADTOF_load
+
+
+def train(config: tune.RayConfig, Model: nn.Module, train_path: str, val_path: str, device: str = "cpu", seed: int | None = None):
+    """ Training function to use with RayTune """
+
+    # Declare device
+    device = device if torch.cuda.is_available() else "cpu"
+
+    # Load the datasets
+    train_loader = ADTOF_load(train_path, batch_size=config["batch_size"], shuffle=True, seed=seed)
+    val_loader = ADTOF_load(train_path, batch_size=config["batch_size"], shuffle=True, seed=seed)
+
+    # Create the model, loss function and optimizer
+    model = Model().to(device)
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = optim.AdamW(model.parameters(), lr=config["lr"], weight_decay=config["weight_decay"], amsgrad=config["amsgrad"])
+    optimizer.zero_grad(set_to_none=True)
+
+    # Start training
+    model.train()
+    for epoch in range(3):
+        train_loss = 0.0
+        for i, data in enumerate(train_loader):
+            # Perform forward, backward and optimization step
+            inputs, labels = data[0].to(device), data[1].to(device)
+            outputs = model(inputs)
+
+            loss = loss_fn(outputs, labels)
+            loss.backward()
+
+            optimizer.backward()
+            optimizer.zero_grad()
+
+            # Print statistics every 2000th mini-batch
+            train_loss += loss.item()
+            if (i+1) % 2000 == 0:
+                print(f"[Epoch {epoch+1}, {i+1}] loss: {train_loss / (i+1) :.4f}")
+
+        # After a training epoch, compute validation performance
+        val_loss = 0.0
+        model.eval()
+        for i, data in enumerate(val_loader):
+            with torch.no_grad():
+                inputs, labels = data[0].to(device), data[1].to(device)
+                outputs = model(inputs)
+
+                loss = loss_fn(outputs, labels)
+                val_loss += loss.item()
+        
+        # Report to RayTune
+        train.report({"loss": val_loss / (i+1)})
+
+    print("Finished training")
