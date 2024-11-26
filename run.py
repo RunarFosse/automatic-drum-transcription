94e531661181ab94c1bf9424712b9ea716208d8b
Runar Fosse <rfosse01@gmail.com>
2024-11-26 11:52:29 +0100

    Lowered weight decay boundaries

--- run.py
+++ run.py
@@ -32,7 +32,7 @@ val_path = "adtof/adtof_yt_validation"
 config = {
     "batch_size": tune.choice([16, 32]),
     "lr": tune.loguniform(1e-4, 1e-2),
-    "weight_decay": tune.loguniform(1e-5, 1e-3),
+    "weight_decay": tune.loguniform(1e-5, 1e-4),
     "amsgrad": tune.choice([True, False]),
 }
 
