789bcf31566244aecf398417c17e99346b7ed54e
Runar Fosse <rfosse01@gmail.com>
2024-11-26 14:31:49 +0100

    Store TensorFlow dataset on CPU

--- datasets.py
+++ datasets.py
@@ -40,13 +40,13 @@ def ADTOF_load(path: Path, batch_size = 1, shuffle = False, transform=None, seed
         # Load the dataset using TensorFlow
         tf_dataset = tf.data.Dataset.load(str(path))
 
-    # Let TensorFlow handle batching and shuffling
-    tf_dataset = tf_dataset.shuffle(buffer_size = batch_size * 25, seed = seed)
-    tf_dataset = tf_dataset.batch(batch_size = batch_size)
-    tf_dataset = tf_dataset.prefetch(tf.data.AUTOTUNE)
+        # Let TensorFlow handle batching and shuffling
+        tf_dataset = tf_dataset.shuffle(buffer_size = batch_size * 25, seed = seed)
+        tf_dataset = tf_dataset.batch(batch_size = batch_size)
+        tf_dataset = tf_dataset.prefetch(tf.data.AUTOTUNE)
 
-    # Wrap the dataset as a PyTorch iterable dataset
-    dataset = TensorFlowDatasetIterable(tf_dataset, transform = transform)
+        # Wrap the dataset as a PyTorch iterable dataset
+        dataset = TensorFlowDatasetIterable(tf_dataset, transform = transform)
 
     # And return it as a PyTorch DataLoader
     dataloader = DataLoader(dataset, batch_size = None)
