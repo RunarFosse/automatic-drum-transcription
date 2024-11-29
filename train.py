937a0d8a94742f6c2496c5d8c24b6cf312aaf85d
Runar Fosse <rfosse01@gmail.com>
2024-11-29 16:21:31 +0100

    Print outputs every now and then

--- train.py
+++ train.py
@@ -63,7 +63,8 @@ def train_model(config: tune.TuneConfig, Model: nn.Module, n_epochs: int, train_
             train_loss += loss.item()
             n_batches_train += 1
         
-        #print(torch.stack((outputs, labels), dim=-1))
+        if epoch % 50 == 0:
+            print(torch.stack((outputs, labels), dim=-1))
 
         # After a training epoch, compute validation performance
         model.eval()
