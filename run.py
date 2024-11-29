ecef265c000eda2373d5f0f749492aa862b241e4
Runar Fosse <rfosse01@gmail.com>
2024-11-29 17:17:29 +0100

    Lowered learning rate

--- run.py
+++ run.py
@@ -36,7 +36,7 @@ val_path = "adtof/adtof_yt_validation"
 config = {
     "batch_size": tune.choice([128]),
     #"batch_size": 1,
-    "lr": tune.loguniform(1e-5, 1e-3),
+    "lr": tune.loguniform(1e-6, 1e-4),
     #"lr": 0.01,
     "weight_decay": tune.loguniform(1e-5, 1e-4),
     #"weight_decay": 0,
