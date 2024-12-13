8d35763547bdc9c169683bab3c5532d571739e41
Runar Fosse <rfosse01@gmail.com>
2024-12-13 11:12:45 +0100

    Implemented Vogl's peak picking algorithm

--- evaluate.py
+++ evaluate.py
@@ -7,10 +7,38 @@ def compute_peaks(activations: torch.Tensor, m: int = 2, o: int = 2, w: int = 2,
     # First pad the data to handle boundary conditions
     padded_activations = F.pad(activations, (m, m), value = 0)
 
-    # Then compute peaks using Vogl's criteria
-    is_peak = torch.ones(2 * m + 1, dtype=torch.bool)
-    for i in range(1, m + 1):
-        is_peak &= activations > padded_activations[i:-i]
-        is_peak &= activations > padded_activations[i:-i]
+    # Then compute sliding windows over each of the activations
+    windows = padded_activations.unfold(dimension = -1, size = 2*m + 1, step = 1)
+
+    # Then compute peaks using Vogl's max and mean criteria
+    is_peak = (activations == torch.amax(windows, dim=-1)) & (activations >= torch.mean(windows, dim=-1) + delta)
     
-    torch.nn.Unfold
+    # Enforce minimum distance between peaks
+    n_batches, n_labels = activations.shape[0:2]
+    filtered_peaks = []
+    for batch in range(n_batches):
+        for label in range(n_labels):
+            peak_indices = torch.where(is_peak[batch][label])[0]
+            last_peak = -(w - 1)
+            for peak in peak_indices:
+                if peak - last_peak > w:
+                    filtered_peaks.append([batch, label, peak])
+                    last_peak = peak
+    
+    # Return the output masked by the peaks
+    mask = torch.zeros_like(activations)
+    mask[*torch.tensor(filtered_peaks).T] = 1
+
+    return activations * mask
+
+if __name__ == "__main__":
+    batch_data = torch.tensor([[
+        [0.1, 0.3, 0.7, 0.4, 0.5, 0.9, 0.2, 0.8, 0.6, 0.4, 0.9, 0.3],
+        [0.2, 0.6, 0.1, 0.4, 0.8, 0.3, 0.7, 0.2, 0.5, 0.9, 0.2, 0.1]
+    ],
+    [
+        [0.0, 0.1, 0.7, 0.2, 0.3, 0.7, 0.9, 0.8, 0.7, 0.7, 0.7, 0.9],
+        [0.0, 0.2, 0.3, 0.2, 0.1, 0.0, 0.0, 0.9, 0.0, 0.9, 0.0, 0.9]
+    ]])
+
+    print(compute_peaks(batch_data))
