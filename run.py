b43987455b6fa638bc34d794ecc95726a43cc5b7
Runar Fosse <rfosse01@gmail.com>
2025-01-16 22:37:49 +0100

    Slightly tweaked hyperparameters

--- run.py
+++ run.py
@@ -35,11 +35,11 @@ train_path = "adtof/adtof_yt_train"
 val_path = "adtof/adtof_yt_validation"
 
 config = {
-    "batch_size": tune.choice([64, 128]),
+    "batch_size": tune.choice([128]),
     #"batch_size": 16,
     "lr": tune.loguniform(1e-5, 1e-3),
     #"lr": 0.01,
-    "weight_decay": tune.loguniform(1e-5, 1e-3),
+    "weight_decay": tune.loguniform(1e-5, 1e-4),
     #"weight_decay": 0,
     "amsgrad": tune.choice([True, False]),
     #"amsgrad": False,
