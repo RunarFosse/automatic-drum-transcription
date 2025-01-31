3b2c79d2b9c57224df0ed91000028b69c0c649e2
Runar Fosse <rfosse01@gmail.com>
2025-01-31 23:27:51 +0100

    Removed bias term from convolutional encoders

--- models.py
+++ models.py
@@ -5,17 +5,17 @@ import torch.nn.functional as F
 class FrameSynchronousCNNEncoder(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=(3, 3), padding=1)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=(3, 3), padding=1, bias=False)
         self.batchnorm1 = nn.BatchNorm2d(32)
-        self.conv2 = nn.Conv2d(32, 32, kernel_size=(3, 3), padding=1)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=(3, 3), padding=1, bias=False)
         self.batchnorm2 = nn.BatchNorm2d(32)
 
         self.pool1 = nn.MaxPool2d(kernel_size=(1, 3))
         self.dropout1 = nn.Dropout2d(p=0.3)
 
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=(3, 3), padding=1)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=(3, 3), padding=1, bias=False)
         self.batchnorm3 = nn.BatchNorm2d(64)
-        self.conv4 = nn.Conv2d(64, 64, kernel_size=(3, 3), padding=1)
+        self.conv4 = nn.Conv2d(64, 64, kernel_size=(3, 3), padding=1), bias=False
         self.batchnorm4 = nn.BatchNorm2d(64)
 
         self.pool2 = nn.MaxPool2d(kernel_size=(1, 3))
