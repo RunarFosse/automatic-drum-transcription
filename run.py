70378d110495272e55e8302983d24249574a55b0
Runar Fosse <rfosse01@gmail.com>
2025-02-02 13:15:16 +0100

    Fixed error on string declaration

--- run.py
+++ run.py
@@ -93,4 +93,4 @@ test_f1_global, test_f1_class = evaluate_model(model, test_path=test_path, devic
 
 print(" ---------- Evaluation of best perfoming model ---------- ")
 print(f"Global F1: {test_f1_global.item():.4f}")
-print(f"Class F1: {[f"{test_f1.item():.4f}" for test_f1 in test_f1_class]}")
+print(f"Class F1: {[f'{test_f1.item():.4f}' for test_f1 in test_f1_class]}")
