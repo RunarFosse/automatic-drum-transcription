8dbe3b7d5563e087c8467b1f7792813548abfcf7
Runar Fosse <rfosse01@gmail.com>
2025-03-01 14:25:30 +0100

    Started implementing parsing, creation and inversion of logarithmically filtered spectrograms

--- preprocess.py
+++ preprocess.py
@@ -62,6 +62,78 @@ def compute_infrequency_weights(dataloader: DataLoader) -> torch.Tensor:
     weights = 1.0 / (-probabilities * torch.log(probabilities) - (1.0 - probabilities) * torch.log(1.0 - probabilities))
     return weights
 
+
+
+def compute_log_filterbank(sr: int = 44100, n_fft: int = 2048, f_min: int = 20, f_max: int = 20000, f_ref: int = 440, bands_per_octave: int = 12, norm: bool = True) -> torch.Tensor:
+    left = torch.floor(torch.log2(torch.tensor(f_min) / f_ref) * bands_per_octave)
+    right = torch.ceil(torch.log2(torch.tensor(f_max) / f_ref) * bands_per_octave)
+
+    # Generate logarithmically spaced frequencies
+    frequencies = 440 * torch.pow(torch.tensor(2), (torch.arange(left, right) / bands_per_octave))
+
+    # Remove any outside the bounds (due to use of torch.floor/ceil)
+    valid_frequencies = torch.logical_and(f_min <= frequencies , frequencies <= f_max)
+    frequencies = frequencies[valid_frequencies]
+
+    # Compute Fourier Transform bins
+    bins = torch.fft.rfftfreq(n=n_fft, d=1/sr)
+    indices = torch.unique(torch.searchsorted(bins, frequencies).clip(1, bins.shape[0] - 1))
+
+    # Create the filterbank
+    filterbank = torch.zeros(size=(indices.shape[0] - 1, bins.shape[0]))
+
+    # And create each filter
+    for i in range(indices.shape[0] - 2):
+        # Compute a start and stop index (forced to be bigger than 1)
+        start, center, stop = indices[i:i+3]
+        if stop - start < 2:
+            center = start
+            stop = start + 1
+
+        # Set the values
+        filterbank[i, start:center] = torch.linspace(0, 1, center  - start)
+        filterbank[i, center:stop] = torch.linspace(1, 0, stop - center)
+
+        # Normalize
+        if norm:
+            filterbank[i, start:stop] /= torch.sum(filterbank[i, start:stop])
+    
+    return filterbank
+
+
+def compute_log_filter_spectrogram(waveform: torch.Tensor, sr: int = 44100, n_fft: int = 2048, win_length: int = 2048, hop_length: int = 441, f_min: int = 20, f_max: int = 20000, power: int = 1, norm: bool = True) -> torch.Tensor:
+    """ Given a waveform, compute a spectrogram with a logarithmic filterbank applied. """
+
+    # If the waveform is stereo, turn it to mono
+    if waveform.shape[0] == 2:
+        waveform = waveform.mean(dim=0)
+ 
+    # Compute a logarithmically spaced filterbank
+    filterbank = compute_log_filterbank(sr=sr, n_fft=n_fft, f_min=f_min, f_max=f_max, norm=norm)
+    
+    # Then, compute STFT
+    spectrogram = torchaudio.transforms.Spectrogram(n_fft=n_fft, win_length=win_length, hop_length=hop_length, power=power)(waveform)
+
+    # Apply logarithimic filters
+    spectrogram = filterbank @ spectrogram
+
+    # Compute log-magnitude
+    spectrogram = torch.log10(spectrogram + 1)
+
+    return spectrogram
+
+def invert_log_filter_spectrogram(spectrogram: torch.Tensor, sr: int = 44100, n_fft: int = 2048, win_length: int = 2048, hop_length: int = 441, f_min: int = 20, f_max: int = 20000, power: int = 1, norm: bool = True) -> torch.Tensor:
+    """ Given a log logarithmically filterbank spectrogram, invert it, and return its waveform. """
+
+    # Invert log magnitude
+    spectrogram = torch.pow(torch.tensor(10), spectrogram) - 1
+    
+    # Remove filters
+    inverse_filterbank = torch.linalg
+
+    grifflim_transform = torchaudio.transforms.GriffinLim(power=power, n_fft=n_fft, win_length=win_length, hop_length=hop_length)
+
+
 def invert_mel_spectrogram(spectrogram: torch.Tensor, n_fft: int = 2048, win_length: int = 2048, hop_length: int = 441, n_mels: int = 84, f_min: int = 20, f_max: int = 20000, norm: str = "slaney", mel_scale: str = "htk", n_iter: int = 32, power: int = 2) -> torch.Tensor:
     """ Given a log mel spectrogram, invert it, and return its waveform. """
 
