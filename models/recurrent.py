7af4a797eced6fe2ce67526aa66788eb01e6cbdd
Runar Fosse <rfosse01@gmail.com>
2025-03-27 15:15:56 +0100

    Set dropout=0.1 in recurrent decoder

--- models/recurrent.py
+++ models/recurrent.py
@@ -4,12 +4,12 @@ import torch.nn.functional as F
 
 
 class RNNDecoder(nn.Module):
-    def __init__(self, num_layers: int = 3, hidden_size: int = 288, use_gru: bool = True):
+    def __init__(self, input_size: int = 576, num_layers: int = 3, hidden_size: int = 288, use_gru: bool = True):
         super().__init__()
         if use_gru:
-            self.rnns = nn.GRU(input_size=576, hidden_size=hidden_size, num_layers=num_layers, bidirectional=True, batch_first=True)
+            self.rnns = nn.GRU(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, bidirectional=True, batch_first=True, dropout=0.1)
         else:
-            self.rnns = nn.LSTM(input_size=576, hidden_size=hidden_size, num_layers=num_layers, bidirectional=True, batch_first=True)
+            self.rnns = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, bidirectional=True, batch_first=True, dropout=0.1)
         self.fc = nn.Linear(2 * hidden_size, 5)
     
     def forward(self, x: torch.Tensor) -> torch.Tensor:
