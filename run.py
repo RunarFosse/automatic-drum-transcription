95d56d0fe72bc9c83b7142902b387ab04ebddbb9
Runar Fosse <rfosse01@gmail.com>
2025-03-27 15:20:39 +0100

    Train RNN

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from torch import optim
 from torch.utils.data import DataLoader
 from ray import init, tune, train
 from time import time
-from models import CNN, ADTOF_FrameRNN, ADTOF_FrameAttention, VisionTransformer
+from models import RNN, CNN, ADTOF_FrameRNN, ADTOF_FrameAttention, VisionTransformer
 from preprocess import compute_normalization, create_transform
 from evaluate import evaluate_model
 from train import train_model
@@ -33,10 +33,10 @@ device = args.device
 seed = int(time())
 
 study = "Architecture"
-experiment = "CRNN"
+experiment = "RNN"
 dataset = "ADTOF-YT"
 
-Model = ADTOF_FrameRNN
+Model = RNN
 
 num_samples = 1
 num_epochs = 100
