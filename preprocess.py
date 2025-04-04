93b908ee9b7b10fbacd91128f9dcf1975b6affab
Runar Fosse <rfosse01@gmail.com>
2025-04-04 11:39:05 +0200

    Fixed wrong .to() position

--- preprocess.py
+++ preprocess.py
@@ -26,7 +26,7 @@ def compute_normalization(train_path: Path, batch_size: int = 1, device: str = "
         std += torch.std(features, dim=(0, 1, 2))
 
     # Return divided over number of batches
-    return mean.to("cpu") / num_batches, std / num_batches.to("cpu")
+    return mean.to("cpu") / num_batches, std.to("cpu") / num_batches
 
 def create_transform(mean: torch.Tensor, std: torch.Tensor, channels_last: bool) -> transforms.Compose:
     """ Create a preprocessing transforms pipeline. """
