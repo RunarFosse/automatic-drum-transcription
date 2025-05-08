Fixed implementing ConcatDataset and fixed normalization

preprocess.py
@@ -1,6 +1,6 @@
 import torch
 import torchaudio
-from torch.utils.data import DataLoader, Dataset, ConcatDataset
+from torch.utils.data import DataLoader, ConcatDataset
 from torchvision import transforms, ops
 from pathlib import Path
 from typing import Tuple, List
@@ -18,7 +18,7 @@ def compute_normalization(train_paths: List[Path]) -> Tuple[torch.Tensor]:
     stack = torch.stack([img for img, _ in train_dataset])
 
     # And compute and return values
-    mean, std = stack.mean((0, 2, 3)), stack.std((0, 2, 3))
+    mean, std = stack.mean(dim=(0, 1, 2)), stack.std(dim=(0, 1, 2))
     return mean, std
 
 def create_transform(mean: torch.Tensor, std: torch.Tensor, channels_last: bool) -> transforms.Compose:
@@ -93,27 +93,3 @@ def invert_mel_spectrogram(spectrogram: torch.Tensor, n_fft: int = 2048, win_len
     pseudo_waveform = grifflim_transform(inverse_waveform)
 
     return pseudo_waveform
-
-
-
-class CompositeDataset(Dataset):
-    """ 
-    Custom datset consisting of several sub-datasets. 
-    Datapoints are randomly sampled with a weighted probability equal to relative dataset size. 
-    """
-    def __init__(self, paths: List[Path]):
-        self.datasets = [torch.load(path) for path in paths]
-        self.sizes = [len(dataset) for dataset in self.datasets]
-    
-    def __len__(self):
-        return sum(self.sizes)
-
-    def __getitem__(self, index: int):
-        # Number of datasets are small, so brute-force loop is trivial
-        dataset_index, prefix = 0, 0
-        while prefix + self.sizes[dataset_index] <= index:
-            prefix += self.sizes[dataset_index]
-            dataset_index += 1
-        
-        return self.datasets[dataset_index][index - prefix]
-

run.py
@@ -1,12 +1,12 @@
 import argparse
 import torch
 from torch import optim
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, ConcatDataset
 from ray import init, tune, train
 from ray.tune.search.optuna import OptunaSearch
 from time import time
 from models import RNN, CNN, ADTOF_FrameRNN, ADTOF_FrameAttention, VisionTransformer
-from preprocess import compute_normalization, create_transform, CompositeDataset
+from preprocess import compute_normalization, create_transform
 from evaluate import evaluate_model
 from train import train_model
 from pathlib import Path
@@ -139,7 +139,7 @@ model = Model(**best_result.config["parameters"])
 model.load_state_dict(state_dict)
 
 # Create a test dataloader and preprocessing transforms
-test_loader = DataLoader(CompositeDataset(test_paths), batch_size=batch_size, num_workers=4, pin_memory=True)
+test_loader = DataLoader(ConcatDataset(map(torch.load, test_paths)), batch_size=batch_size, num_workers=4, pin_memory=True)
 transforms = create_transform(mean=feature_mean, std=feature_std, channels_last=True)
 
 # And evaluate it

train.py
@@ -1,12 +1,12 @@
 import torch
 import torch.nn.functional as F
 from torch import nn, optim
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, ConcatDataset
 
 from ray import train, tune
 from ray.train import Checkpoint
 
-from preprocess import compute_infrequency_weights, create_transform, CompositeDataset
+from preprocess import compute_infrequency_weights, create_transform
 from evaluate import compute_peaks, compute_predictions, f_measure
 
 from tempfile import TemporaryDirectory
@@ -27,8 +27,8 @@ def train_model(config: tune.TuneConfig):
     print(f"Training: Can use CUDA: {torch.cuda.is_available()}")
 
     # Load the datasets into dataloaders
-    train_loader = DataLoader(CompositeDataset(config["train_paths"]), shuffle=True, batch_size=config["batch_size"], num_workers=4, pin_memory=True)
-    val_loader = DataLoader(CompositeDataset(config["val_paths"]), shuffle=True, batch_size=config["batch_size"], num_workers=4, pin_memory=True)
+    train_loader = DataLoader(ConcatDataset(map(torch.load, config["train_paths"])), shuffle=True, batch_size=config["batch_size"], num_workers=4, pin_memory=True)
+    val_loader = DataLoader(ConcatDataset(map(torch.load, config["val_paths"])), shuffle=True, batch_size=config["batch_size"], num_workers=4, pin_memory=True)
 
     # Create a transform preprocessing pipeline
     transforms = create_transform(**config["transforms"], channels_last=True)