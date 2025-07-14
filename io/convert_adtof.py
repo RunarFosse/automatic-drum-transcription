a8e8764a89b9b1868eaa381fcdf45376fca30dc8
Runar Fosse <rfosse01@gmail.com>
2025-07-14 15:22:36 +0200

    Updated adtof conversion to automatically convert each split

--- io/convert_adtof.py
+++ io/convert_adtof.py
@@ -8,45 +8,42 @@ from pathlib import Path
 
 
 if __name__ == "__main__":
-    # Declare an argument parser for this file
-    import argparse
-    parser = argparse.ArgumentParser("datasets.py")
-    parser.add_argument("dataset", help="The dataset to convert", type=str)
-    args = parser.parse_args()
+    # Declare the path to the dataset directory
+    path = Path(__file__).resolve().parent.parent / "data" / "adtof"
 
-    # Get the path of the folder
-    path = Path(__file__).resolve().parent / args.dataset
+    # Split into Train, Validation, Test
+    for split in ["train", "validation", "test"]:
 
-    print("\033[96m", "Loading TensorFlow dataset from disk", "\033[0m", sep="")
+        print("\033[96m", f"    Loading TensorFlow {split} dataset from disk", "\033[0m", sep="")
 
-    # Load the dataset
-    tf_dataset = tf.data.Dataset.load(str(path))
+        # Load the dataset
+        tf_dataset = tf.data.Dataset.load(str(path / f"adtof_yt_{split}"))
 
-    print("\033[96m", "Loading data into lists", "\033[0m", sep="")
+        print("\033[96m", "     Loading data into lists", "\033[0m", sep="")
 
-    # Store all features and labels
-    features, labels = [], []
-    for data, label in tf_dataset.as_numpy_iterator():
-        features.append(data["x"])
-        labels.append(label)
-    features, labels = np.array(features), np.array(labels)
+        # Store all features and labels
+        features, labels = [], []
+        for data, label in tf_dataset.as_numpy_iterator():
+            features.append(data["x"])
+            labels.append(label)
+        features, labels = np.array(features), np.array(labels)
 
-    print("\033[96m", "Creating tensor datasets", "\033[0m", sep="")
+        print("\033[96m", "     Creating tensor datasets", "\033[0m", sep="")
 
-    # Turn them into a Pytorch tensor dataset
-    dataset = TensorDataset(torch.tensor(features), torch.tensor(labels))
+        # Turn them into a Pytorch tensor dataset
+        dataset = TensorDataset(torch.tensor(features), torch.tensor(labels))
 
-    print("\033[96m", "Storing dataset to disk", "\033[0m", sep="")
+        # And store the dataset to the disk under the first path
+        new_path = (path / f"adtof_yt_{split}").with_suffix(".pt")
+        print("\033[96m", "     Storing ", "\033[0m", f"{new_path.name}", "\033[96m", " to disk", "\033[0m", sep="")
+        torch.save(dataset, new_path)
 
-    # And store the dataset to the disk under the first path
-    torch.save(dataset, path.with_suffix(".pt"))
-
-    print("\033[95m", "Finished!", "\033[0m", sep="")
+        print("\033[95m", "     Finished!", "\033[0m", sep="")
     
-    # Load dataset and verify that everything is correct
-    dataset = torch.load(path.with_suffix(".pt"))
-    print("\033[92m", "Final dataset contains ", "\033[0m", len(dataset), "\033[92m", " entries", "\033[0m", sep="")
-    print("\033[92m", "Each entry has features of shape: ", "\033[0m", dataset[0][0].shape, "\033[92m", ", and labels of shape: ", "\033[0m", dataset[0][1].shape, sep="")
+        # Load dataset and verify that everything is correct
+        dataset = torch.load(new_path)
+        print("\033[92m", "     Final dataset contains ", "\033[0m", len(dataset), "\033[92m", " entries", "\033[0m", sep="")
+        print("\033[92m", "     Each entry has features of shape: ", "\033[0m", dataset[0][0].shape, "\033[92m", ", and labels of shape: ", "\033[0m", dataset[0][1].shape, sep="")
 
     # Verify that dataloaders work
     dataloader = DataLoader(dataset, batch_size=16)
